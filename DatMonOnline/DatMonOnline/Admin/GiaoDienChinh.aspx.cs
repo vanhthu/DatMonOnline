d5275ae4b5d54c60adfc15c174fc375dfcd5f8be
vanhthu <voanhthu743@gmail.com>
2024-01-25 15:45:12 +0700

    chỉnh sửa trang admin

---
+++ DatMonOnline/DatMonOnline/Admin/GiaoDienChinh.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DatMonOnline.Admin
+{
+    public partial class GiaoDienChinh : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
