f87f1e7c4e953b38cc2b6ea49e0f954329eca86d
vanhthu <voanhthu743@gmail.com>
2024-01-25 14:55:30 +0700

    xóa bỏ slider ra khỏi trang menu

--- DatMonOnline/DatMonOnline/NguoiDung/NguoiDung.Master.cs
+++ DatMonOnline/DatMonOnline/NguoiDung/NguoiDung.Master.cs
@@ -11,6 +11,19 @@ namespace DatMonOnline.NguoiDung
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("TrangChu.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+
+                form1.Attributes.Remove("class");
+                // load control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+
+                panelSlider.Controls.Add(sliderUserControl);
+            }
 
         }
     }
--- DatMonOnline/DatMonOnline/NguoiDung/NguoiDung.Master.designer.cs
+++ DatMonOnline/DatMonOnline/NguoiDung/NguoiDung.Master.designer.cs
@@ -33,13 +33,13 @@ namespace DatMonOnline.NguoiDung
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// SliderUserControl control.
+        /// panelSlider control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::DatMonOnline.NguoiDung.SliderUserControl SliderUserControl;
+        protected global::System.Web.UI.WebControls.Panel panelSlider;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
