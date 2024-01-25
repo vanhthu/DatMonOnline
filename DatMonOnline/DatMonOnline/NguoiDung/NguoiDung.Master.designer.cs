7b95136aaf33273920250cad80921db34fbdcc65
vanhthu <voanhthu743@gmail.com>
2024-01-25 14:38:35 +0700

    chỉnh sửa lại slider cho trang menu

--- DatMonOnline/DatMonOnline/NguoiDung/NguoiDung.Master.designer.cs
+++ DatMonOnline/DatMonOnline/NguoiDung/NguoiDung.Master.designer.cs
@@ -33,6 +33,15 @@ namespace DatMonOnline.NguoiDung
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// SliderUserControl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::DatMonOnline.NguoiDung.SliderUserControl SliderUserControl;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
