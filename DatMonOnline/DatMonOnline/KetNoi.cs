54c59b70e82a12480b1fcf1d45ed22d0d3b5c47b
vanhthu <voanhthu743@gmail.com>
2024-01-25 21:08:25 +0700

    tạo user và pass cho admin

---
+++ DatMonOnline/DatMonOnline/KetNoi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace DatMonOnline
+{
+    public class KetNoi
+    {
+        public static string LayChuoiKetNoi()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+
+    }
+}
