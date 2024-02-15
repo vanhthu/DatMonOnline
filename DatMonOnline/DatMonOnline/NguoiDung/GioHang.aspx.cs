27c844edabb5ffbe8cb2f8e01afb47ec33f49908
vanhthu <voanhthu743@gmail.com>
2024-02-16 00:36:02 +0700

    .

--- DatMonOnline/DatMonOnline/NguoiDung/GioHang.aspx.cs
+++ DatMonOnline/DatMonOnline/NguoiDung/GioHang.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,24 @@ namespace DatMonOnline.NguoiDung
 {
     public partial class GioHang : System.Web.UI.Page
     {
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                if (Session["userID"] == null)
+                {
+                    Response.Redirect("DangNhap.aspx");
+                }
+                else
+                {
 
+                }
+            }
+            
         }
 
         protected void repeatSanPhamGioHang_ItemCommand(object source, RepeaterCommandEventArgs e)
@@ -22,6 +39,25 @@ namespace DatMonOnline.NguoiDung
         protected void repeatSanPhamGioHang_ItemDataBound(object sender, RepeaterItemEventArgs e)
         {
 
+        }
+        // lấy thông tin
+        void LaySanPhamTuGioHang()
+        {
+            cn = new SqlConnection(KetNoi.LayChuoiKetNoi());
+            cmd = new SqlCommand("GIOHANG_CRUD", cn);
+            cmd.Parameters.AddWithValue("@action", "SELECT");            
+            cmd.Parameters.AddWithValue("@userID", Session["userID"]);
+            cmd.CommandType = CommandType.StoredProcedure;
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            repeatSanPhamGioHang.DataSource = dt;
+            if (dt.Rows.Count == 0)
+            {
+                
+            }
+            repeatSanPhamGioHang.DataBind();
+            
         }
     }
 }
